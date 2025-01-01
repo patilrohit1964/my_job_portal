3ab2874d0753b5b8734d24d93fdff9d040668ce0
rohitinfikey <rohit.patil@infikeytechnologies.com>
2025-01-01 23:56:15 +0530

    setup all

---
+++ server/models/userSchema.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      typeof: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "recruiter"],
+      required: true,
+    },
+    profile: {
+      bio: {
+        type: String,
+      },
+      skills: [{ type: String }],
+      resume: { type: String },
+      resumeOriginalName: { type: String },
+      company: { type: mongoose.Schema.Types.ObjectId, ref: "Company" }, //oonly for company
+      profilePhoto: {
+        type: String,
+        default: "", //default profile photo name.
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
