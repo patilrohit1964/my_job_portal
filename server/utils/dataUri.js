a7372076ad6b9ac45b6843bb31e2db1579be0077
patilrohit1964 <patilrohit19649@gmail.com>
2025-01-27 23:45:24 +0530

    added logout api

--- server/utils/dataUri.js
+++ server/utils/dataUri.js
@@ -2,7 +2,6 @@ const DataUriParser = require("datauri/parser");
 const path = require("path");
 
 const getDataUri = (file) => {
-    console.log(file,"iam file")
   const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
   return parser.format(extName, file.buffer);
