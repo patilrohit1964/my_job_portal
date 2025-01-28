26bc9f34e8b016ce6372fd3bc741eb4552e23340
patilrohit1964 <patilrohit19649@gmail.com>
2025-01-28 23:47:12 +0530

    show jobs

--- client/src/components/LatestJobs.jsx
+++ client/src/components/LatestJobs.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import LatestJobCards from './LatestJobCards'
+import { useSelector } from 'react-redux'
 
 const random = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const LatestJobs = () => {
+    const { allJobs } = useSelector(state => state.jobSlice)
     return (
         <div className='max-w-7xl mx-auto my-20'>
             <h1 className='text-4xl font-bold'><span className='text-[#6138c2]'>Latest & Top Job Openings</span>Job Opening</h1>
             {/* job card */}
             <div className='grid grid-cols-3 gap-4 my-5'>
                 {
-                    random.slice(0, 6).map((item, index) => <LatestJobCards key={index} />)
+                    allJobs?.length > 0 && allJobs?.slice(0, 6)?.map((job, index) => <LatestJobCards key={job._id} job={job} />)
                 }
             </div>
         </div>
