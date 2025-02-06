490e1afcc92b4a42e44b070487ff63d3cd586c3a
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-06 23:21:08 +0530

    added job post

--- client/src/components/admin/PostJob.jsx
+++ client/src/components/admin/PostJob.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
+import Navbar from '../shared/Navbar'
+import { Button } from '../ui/button'
 
 const PostJob = () => {
     const [input, setInput] = useState({
@@ -17,99 +19,111 @@ const PostJob = () => {
     const handleInputChange = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value })
     }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(input)
+    }
     return (
-        <div className='flex items-center justify-center w-screen my-5'>
-            <div>
-                <Label>Title</Label>
-                <Input 
-                    type="text" 
-                    name="title" 
-                    value={input.title}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
+        <>
+            <Navbar />
+            <div className='flex items-center justify-center w-screen my-10 flex-col'>
+                <div className='grid grid-cols-2 gap-2'>
+                    <div>
+                        <Label>Title</Label>
+                        <Input
+                            type="text"
+                            name="title"
+                            value={input.title}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Description</Label>
+                        <Input
+                            type="text"
+                            name="description"
+                            value={input.description}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Requirements</Label>
+                        <Input
+                            type="text"
+                            name="requirements"
+                            value={input.requirements}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Salary</Label>
+                        <Input
+                            type="number"
+                            name="salary"
+                            value={input.salary}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Location</Label>
+                        <Input
+                            type="text"
+                            name="location"
+                            value={input.location}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Job Type</Label>
+                        <Input
+                            type="text"
+                            name="jobType"
+                            value={input.jobType}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Experience Level</Label>
+                        <Input
+                            type="text"
+                            name="experience"
+                            value={input.experience}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Position</Label>
+                        <Input
+                            type="number"
+                            name="position"
+                            value={input.position}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                    <div>
+                        <Label>Company ID</Label>
+                        <Input
+                            type="text"
+                            name="companyId"
+                            value={input.companyId}
+                            onChange={handleInputChange}
+                            className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1"
+                        />
+                    </div>
+                </div>
+                <div className='my-5 w-80'>
+                    <Button className="w-full" onClick={handleSubmit}>Post Job</Button>
+                </div>
             </div>
-            <div>
-                <Label>Description</Label>
-                <Input 
-                    type="text" 
-                    name="description"
-                    value={input.description}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-            <div>
-                <Label>Requirements</Label>
-                <Input 
-                    type="text" 
-                    name="requirements"
-                    value={input.requirements}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-            <div>
-                <Label>Salary</Label>
-                <Input 
-                    type="number" 
-                    name="salary"
-                    value={input.salary}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-            <div>
-                <Label>Location</Label>
-                <Input 
-                    type="text" 
-                    name="location"
-                    value={input.location}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-            <div>
-                <Label>Job Type</Label>
-                <Input 
-                    type="text" 
-                    name="jobType"
-                    value={input.jobType}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-            <div>
-                <Label>Experience Level</Label>
-                <Input 
-                    type="text" 
-                    name="experience"
-                    value={input.experience}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-            <div>
-                <Label>Position</Label>
-                <Input 
-                    type="number" 
-                    name="position"
-                    value={input.position}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-            <div>
-                <Label>Company ID</Label>
-                <Input 
-                    type="text" 
-                    name="companyId"
-                    value={input.companyId}
-                    onChange={handleInputChange}
-                    className="focus-visible:ring-offset-0 foucs-visible:ring-0 my-1" 
-                />
-            </div>
-        </div>
+        </>
     )
 }
 
