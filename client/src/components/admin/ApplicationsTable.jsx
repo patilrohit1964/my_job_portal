50e943fdda24c55387dd81d82ed85b9e5cc36405
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-08 23:31:09 +0530

    inc status update func

--- client/src/components/admin/ApplicationsTable.jsx
+++ client/src/components/admin/ApplicationsTable.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { MoreHorizontal } from 'lucide-react'
+import { useSelector } from 'react-redux'
+import moment from 'moment';
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import { APPLICATION_API_END_POINT } from '@/utils/constants'
+import { useParams } from 'react-router-dom'
 
 const short = ["re", "send"]
 const ApplicationsTable = () => {
+    const { id } = useParams();
+    const { applications } = useSelector(state => state?.applicationSlice);
+    const statusHandler = async (status, id) => {
+        try {
+            const { data } = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}`, "data", {
+                withCredentials: true
+            })
+            if (data?.status) {
+                toast.success(data?.message);
+            }
+        } catch (error) {
+            toast.error(error?.message);
+            console.error(error);
+        }
+    }
     return (
         <div>
             <Table>
@@ -20,24 +41,28 @@ const ApplicationsTable = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell>Fullname</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Contact</TableCell>
-                        <TableCell>Resume</TableCell>
-                        <TableCell>Date</TableCell>
-                        <TableCell className="float-right cursor-pointer">
-                            <Popover>
-                                <PopoverTrigger><MoreHorizontal /></PopoverTrigger>
-                                <PopoverContent className="w-32"></PopoverContent>
-                                {short.map((status, index) => (
-                                    <div key={index} className='flex w-fit my-2 cursor-pointer items-center'>
-                                        <span>{status}</span>
-                                    </div>
-                                ))}
-                            </Popover>
-                        </TableCell>
-                    </TableRow>
+                    {
+                        applications && applications?.applications?.map((item) => (
+                            <TableRow key={item?._id}>
+                                <TableCell>{item?.applier?.fullname}</TableCell>
+                                <TableCell>{item?.applier?.email}</TableCell>
+                                <TableCell>{item?.applier?.phoneNumber}</TableCell>
+                                <TableCell><a href={`${item?.applier?.profile?.resume}`} target='_blank' className="hover:text-blue-500">{item?.applier?.profile?.resumeOriginalName}</a></TableCell>
+                                <TableCell>{moment(item?.createdAt).format("YYYY-MMM-DD")}</TableCell>
+                                <TableCell className="float-right cursor-pointer">
+                                    <Popover>
+                                        <PopoverTrigger><MoreHorizontal /></PopoverTrigger>
+                                        <PopoverContent className="w-32"></PopoverContent>
+                                        {short.map((status, index) => (
+                                            <div key={index} className='flex w-fit my-2 cursor-pointer items-center'>
+                                                <span>{status}</span>
+                                            </div>
+                                        ))}
+                                    </Popover>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
                 </TableBody>
             </Table>
         </div>
